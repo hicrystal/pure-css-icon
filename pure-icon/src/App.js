598fba69f2a8897a91942998eb51c78873764593
Crystal Gao <jinggao@ebay.com>
2019-08-12 18:54:09 +0800

    add check icon

--- pure-icon/src/App.js
+++ pure-icon/src/App.js
@@ -3,22 +3,20 @@ import React from 'react';
 import './App.css';
 import './styles/App.scss'
 
-
-const iconClassArr = ['burg','home','user','settings','mail','message','search','clock','plus', 'minus', 'times', 'divide', 'arrow-up', 'arrow-down', 'arrow-left', 'arrow-right', 'play', 'pause', 'stop','record','next','prev','first','last'];
+const iconClassArr = ['burg','home','user','settings','mail','message','search','clock','plus', 'minus', 'times', 'divide', 'arrow-up', 'arrow-down', 'arrow-left', 'arrow-right', 'play', 'pause', 'stop','record','next','prev','first','last','check'];
 function App() {
-  const item = iconClassArr.map(element=> 
-     <li key={element}>
-     <span className={element +' icon'} ></span>
-     <span>{element}</span>
+     const item = iconClassArr.map((element,index)=>
+      <div>
+     <li key={element} className={element +' icon'}>
      </li>
-  )
+     <span key={element +'-' + index} >{element}</span>
+     </div>
+     )
+     
   return (
     <div className="App">
      <ul>{item}</ul>
-
      </div>
   );
 }
-
-
 export default App;
